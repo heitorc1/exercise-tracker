d1e07d617397c5eef7ef639e26a33e7cae534c8c
Heitor Carneiro <heitorcarneiro1@gmail.com>
2023-10-18 10:59:52 -0300

    fix: build

--- apps/backend/src/infra/db/index.ts
+++ apps/backend/src/infra/db/index.ts
@@ -1,7 +1,9 @@
 import * as Database from 'better-sqlite3';
+import type { Database as DatabaseType } from 'better-sqlite3';
 import { createDb } from './createDb';
 
-const db = new Database('exercise.db');
+const db: DatabaseType = new Database('exercise.db');
+db.pragma('journal_mode = WAL');
 
 createDb(db);
 
